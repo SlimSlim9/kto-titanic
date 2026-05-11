ddc469a41ebe6d2df15582c178c139ffd2377ab0
SlimSlim9 <ziouche.slimane@gmail.com>
2026-05-12 00:56:55 +0200

    fix :dockerfile

--- src/titanic/api/auth.py
+++ src/titanic/api/auth.py
@@ -6,7 +6,7 @@ import jwt
 from jwt import PyJWKClient
 from jwt.exceptions import ExpiredSignatureError, InvalidTokenError, InvalidAudienceError
 
-# On définit le schéma de sécurité (Bearer Token)
+# On éfinit le schéma de sécurité (Bearer Token)
 security = HTTPBearer()
 
 def verify_token(required_scope: str) -> Callable:
